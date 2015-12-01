27b43f57b59adcf93c84143730904730e08b599b
billyeh <billwyeh@gmail.com>
2015-12-01 13:38:46 -0800

    done with api

--- mobile/src/main/java/com/example/eric/guitarheroes/HomeActivity.java
+++ mobile/src/main/java/com/example/eric/guitarheroes/HomeActivity.java
@@ -1,31 +1,59 @@
 package com.example.eric.guitarheroes;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
 public class HomeActivity extends AppCompatActivity {
+
+  public static GuitarPartyClient guitarPartyClient = new GuitarPartyClient();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.content_home);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
+//    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//    fab.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
+//      }
+//    });
+
+    AsyncHttpResponseHandler handler = new JsonHttpResponseHandler() {
       @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+      public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        try {
+          Log.d("HEROES", response.toString());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
-    });
+    };
+    guitarPartyClient.get("songs/5/", new RequestParams(), handler);
   }
 
   @Override
